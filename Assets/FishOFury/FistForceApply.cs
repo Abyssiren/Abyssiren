e405f2fa53122110f0ad4aa2a992ffda5f9dd30e
pintianz <pintianz@yahoo.com>
2017-01-22 11:24:37 -0500

    Remove forcefield from fist movement
    PT

--- Assets/FishOFury/FistForceApply.cs
+++ Assets/FishOFury/FistForceApply.cs
@@ -4,20 +4,7 @@ using UnityEngine;
 
 public class FistForceApply : MonoBehaviour {
 	public static float forceAmount = 10000000f;
-	public forceField forceField;
-	public GameObject movingParentObj;
-	public Vector3 oldPosition;
 
-	void Start(){
-		oldPosition = (movingParentObj.transform.position);
-	}
-	void FixedUpdate(){
-		//Debug.Log ("VELO - "+GetComponent<Rigidbody>().velocity.magnitude);
-		if((movingParentObj.transform.position - oldPosition).magnitude/Time.fixedDeltaTime >1f){
-			forceField.turnOnForceField((movingParentObj.transform.position - oldPosition).normalized);
-		}
-		oldPosition = (movingParentObj.transform.position);
-	}
 	void OnCollisionEnter(Collision col)
 	{
 		if (col.gameObject.tag == "Fish") {
