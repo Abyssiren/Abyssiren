46823fa8d980c93488297e575f253fd60d9d7803
pintianz <pintianz@yahoo.com>
2017-01-22 06:27:37 -0500

    PlayerHealthDisplay

---
+++ Assets/Scripts/playerHealthDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHealthDisplay : MonoBehaviour {
+
+	public PlayerHealth playerHealth;
+	private Image img;
+	// Use this for initialization
+	void Start () {
+		img = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		img.fillAmount = playerHealth.currHealth / playerHealth.maxHealth;
+	}
+}
