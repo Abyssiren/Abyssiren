57ccf8824761da2e149c79e057983637d02a43aa
pintianz <pintianz@yahoo.com>
2017-01-21 21:11:15 -0500

    Color Util and Color Display Bar
    Color Util and Color Display Bar -PT

---
+++ Assets/Coloring/ColorUtilTester.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorUtilTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		List<ColorEnum> colorList = ColorUtil.getListOfAvailableColor ();
+		for (int i = 0; i < colorList.Count; i++) {
+			Debug.Log ("ColorList - " + i+ " - "+colorList[i]);
+		}
+		Debug.Log ("Random Color - "+ColorUtil.getRandomAvailableColor ());
+		Debug.Log ("Random Color - "+ColorUtil.getRandomAvailableColor ());
+		Debug.Log ("Random Color - "+ColorUtil.getRandomAvailableColor ());
+		Debug.Log ("Random Color - "+ColorUtil.getRandomAvailableColor ());
+		for (int i = 0; i < ColorUtil.MaxColorAllowed; i++) {
+			Debug.Log ("rangeFromKey - " + i+ " - "+ColorUtil.getMaxVolumeRangeFromKey(i));
+		}
+		for (float i = 0.01f; i < 1f; i = i+0.1f) {
+			Debug.Log ("getColorFromVolume - " + i+ " - "+ColorUtil.getColorFromVolume(i));
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
