e401cdc4aeff83e7986bfcaa5fe6118768cdc37d
pintianz <pintianz@yahoo.com>
2017-01-22 00:34:03 -0500

    Add apply force script to fists
    PT

---
+++ Assets/FishOFury/FistForceApply.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistForceApply : MonoBehaviour {
+	public static float forceAmount = 10000000f;
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Fish") {
+			Debug.Log ("COLLIDED - "+col.gameObject.name);
+			col.rigidbody.AddForce (gameObject.GetComponent<Rigidbody>().velocity.normalized * Time.deltaTime * forceAmount);
+		}
+	}
+}
